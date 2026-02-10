9468207f0484b7388ae209903d296a6656398528
bhumpallipavan <125298907+bhumpallipavan@users.noreply.github.com>
2026-02-10 12:21:24 +0530

    Implement CompanyClient to fetch companies and jobs

---
+++ ReportingService/src/main/java/com/placement/reporting/client/CompanyClient.java
@@ -0,0 +1,32 @@
+package com.placement.reporting.client;
+
+import com.placement.reporting.dto.CompanyDto;
+import com.placement.reporting.dto.JobDto;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class CompanyClient {
+
+    private final RestTemplate restTemplate;
+
+    public CompanyClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public List<CompanyDto> getAllCompanies() {
+        String url = "http://localhost:8083/api/companies/list";
+        CompanyDto[] response = restTemplate.getForObject(url, CompanyDto[].class);
+        return response != null ? Arrays.asList(response) : List.of();
+    }
+
+    public List<JobDto> getAllJobs() {
+        String url = "http://localhost:8083/api/jobs/list";
+        JobDto[] response = restTemplate.getForObject(url, JobDto[].class);
+        return response != null ? Arrays.asList(response) : List.of();
+    }
+}
+
